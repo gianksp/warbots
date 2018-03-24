d7eb2cc454a5e8e2bf4afa8012c0e5bf57edd506
Giancarlo Sanchez <g.sanchez@glofox.com>
2018-03-24 14:39:23 +0000

    Diagram updated

--- web/src/index.js
+++ web/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import 'bulma/css/bulma.css'
 import { Routes } from './Routes/Routes'
 
-const title = "Warbots"
 class App extends React.Component {
     render() {
         return (
